fcd4841283eff86700f655018003f7d6d3d80124
IsaltinaPepete <isaltinabrizito@gmail.com>
2024-01-06 03:10:40 -0800

    task 005

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-for i in range(0, 100):
-    if i == 99:
-        print(f"{i}")
+for i in range(0, 100)
+        print("{}".format(number))
     else:
-        print(f"{i:02}", end=", ")
+        print("{:02}".format(number), end=", ")

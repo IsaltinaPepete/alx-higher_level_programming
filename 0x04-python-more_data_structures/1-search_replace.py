12e78e8a8f6f91bec08a68ea791dfb8f80597b96
IsaltinaPepete <isaltinabrizito@gmail.com>
2024-01-14 03:19:28 -0800

    task 1

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 
 def search_replace(my_list, search, replace):
+
     new_list = []
-    for i in range(len(my_list)):
-        if my_list[i] == search:
+    for num in my_list:
+        if num == search:
             new_list.append(replace)
         else:
-            new_list.append(my_list[i])
+            new_list.append(num)
     return new_list
 

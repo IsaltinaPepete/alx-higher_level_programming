933040e7a3f00bfd7ad880e8761b3a21f19daa0a
IsaltinaPepete <isaltinabrizito@gmail.com>
2024-01-26 03:54:39 -0800

    task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+"""Define a class square"""
+
+
+class Square:
+    """This is an empty class Square that defines a square."""
+    def __init__(self, size):
+        self.__size = size

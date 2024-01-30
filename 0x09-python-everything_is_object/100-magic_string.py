89b3aff0dc5116c5c87a394ab762d2a415f19899
IsaltinaPepete <isaltinabrizito@gmail.com>
2024-01-30 04:04:03 -0800

    task

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,3 +1,4 @@
 def magic_string():
     magic_string.count = getattr(magic_string, 'count', 0) + 1
-    return ("BestSchool" + (", " if magic_string.count > 1 else "")) * magic_string.count
+    return (("BestSchool" + (", " if magic_string.count > 1 else ""))
+            * magic_string.count)

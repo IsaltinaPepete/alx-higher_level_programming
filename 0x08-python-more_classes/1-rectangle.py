74b970a038c72aa3ac7c4c3a1882bc16b8d3b1b1
IsaltinaPepete <isaltinabrizito@gmail.com>
2024-02-01 01:09:20 -0800

    task 1

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -11,13 +11,13 @@ class Rectangle:
             width (int): The width of the new rectangle
             height (int): The height of the new rectangle.
         """
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
         """Get width"""
-        return self.width
+        return self.__width
     
     @width.setter
     def width(self, value):

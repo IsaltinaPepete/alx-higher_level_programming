78a827b76d49dbc90fb3862d8efbb13f32b130e3
IsaltinaPepete <isaltinabrizito@gmail.com>
2024-02-07 08:47:20 +0000

    task 2

---
+++ 0x0A-python-inheritance/0-read_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Defines a read_file function"""
+
+
+def read_file(filename=""):
+    """read a file 
+
+    Args:
+        filename(string)- the name of file
+    """
+    with open(filename, 'r', encoding="utf-8") as f:
+        print(f.read())

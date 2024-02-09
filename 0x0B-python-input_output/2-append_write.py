7c1b45434c5d18f83ac88b6f01a3a1df0965ac85
IsaltinaPepete <isaltinabrizito@gmail.com>
2024-02-09 15:15:21 +0000

    task 2

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""""Defines a method"""
+
+def append_write(filename="", text=""):
+    """define a method append
+    Args:
+        filename(string)- name of file
+        text(string) - the text
+    """
+    with(filename, "a+", encoding="utf-8") as f:
+        f.seek(0, 2)
+        return f.write(text)

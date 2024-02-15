e0385f87b8fd7fec4299d124e89eba5b301af8e6
IsaltinaPepete <isaltinabrizito@gmail.com>
2024-02-15 17:13:32 +0000

    display

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -76,4 +76,4 @@ class Rectangle(Base):
         for i in range(self.height):
             for j in range(self.width):
                 print("#", end="")
-            print()
+            print("")

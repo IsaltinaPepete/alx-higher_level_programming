f913c9c5d817c70849f6243279e364833c23644e
IsaltinaPepete <isaltinabrizito@gmail.com>
2024-02-16 07:38:15 -0800

    add dictionary method

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -72,3 +72,15 @@ class Square(Rectangle):
                     self.x = v
                 elif k == "y":
                     self.y = v
+
+    def to_dictionary(self):
+        """dictionary method
+
+        Return: a dictionary
+        """
+        return {
+                "id": self.id,
+                "size": self.size,
+                "x": self.x,
+                "y": self.y
+                }
